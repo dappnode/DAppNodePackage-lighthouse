4374b9fbbc15080a93b5d48935068765866530e9
Pablo <mendez4a@gmail.com>
2022-10-19 13:00:42 +0200

    Implement stakers UI support

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -3,6 +3,37 @@
 # Concatenate EXTRA_OPTS string
 [[ -n "$CHECKPOINT_SYNC_URL" ]] && EXTRA_OPTS="${EXTRA_OPTS} --checkpoint-sync-url=${CHECKPOINT_SYNC_URL}"
 
+case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET in
+"geth.dnp.dappnode.eth")
+    HTTP_ENGINE="http://geth.dappnode:8551"
+    ;;
+"nethermind.dnp.dappnode.eth")
+    HTTP_ENGINE="http://nethermind.dappnode:8551"
+    ;;
+"erigon.dnp.dappnode.eth")
+    HTTP_ENGINE="http://erigon.dappnode:8551"
+    ;;
+"besu.dnp.dappnode.eth")
+    HTTP_ENGINE="http://besu.dappnode:8551"
+    ;;
+*)
+    echo "Unknown value for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET: $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET"
+    HTTP_ENGINE=$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET
+    ;;
+esac
+
+# MEVBOOST: https://lighthouse-book.sigmaprime.io/builders.html
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" == "true" ]; then
+    echo "MEVBOOST is enabled"
+    MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
+    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
+        EXTRA_OPTS="${EXTRA_OPTS} --builder=${MEVBOOST_URL}"
+    else
+        echo "MEVBOOST is enabled but ${MEVBOOST_URL} is not reachable"
+        curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} is not available" --data-urlencode 'body=Make sure the mevboost is available and running'
+    fi
+fi
+
 exec lighthouse \
     --debug-level $DEBUG_LEVEL \
     --network mainnet \
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,48 +1,17 @@
 #!/bin/bash
 
-CLIENT="lighthouse"
 NETWORK="mainnet"
 VALIDATOR_PORT=3500
-VALIDATORS_FILE="/root/.lighthouse/validators/validator_definitions.yml"
-WEB3SIGNER_API="http://web3signer.web3signer.dappnode:9000"
 
-# - Docs: https://lighthouse-book.sigmaprime.io/validator-web3signer.html
-# - FORMAT for each new pubkey:
-# - enabled: true
-#   voting_public_key: "0xa5566f9ec3c6e1fdf362634ebec9ef7aceb0e460e5079714808388e5d48f4ae1e12897fed1bea951c17fa389d511e477"
-#   type: web3signer
-#   url: "https://my-remote-signer.com:1234"
-#   root_certificate_path: /home/paul/my-certificates/my-remote-signer.pem
-function write_validator_definitions() {
-    # Remove validator_definitions.yml file
-    [ -f "${VALIDATORS_FILE}" ] && rm -rf "${VALIDATORS_FILE}"
-    # Create validators file if not exist
-    [ ! -d "/root/.lighthouse/validators" ] && mkdir -p /root/.lighthouse/validators
-    [ ! -f "${VALIDATORS_FILE}" ] && touch "${VALIDATORS_FILE}"
-
-    for PUBLIC_KEY in "${PUBLIC_KEYS_WEB3SIGNER[@]}"; do
-        if [ -n "${PUBLIC_KEY}" ]; then
-            echo "${INFO} adding public key: $PUBLIC_KEY"
-            echo -en "- enabled: true\n  voting_public_key: \"${PUBLIC_KEY}\"\n  type: web3signer\n  url: \"${HTTP_WEB3SIGNER}\"\n" >>${VALIDATORS_FILE}
-        else
-            echo "${WARN} empty public key"
-        fi
-    done
-}
-
-WEB3SIGNER_RESPONSE=$(curl -s -w "%{http_code}" -X GET -H "Content-Type: application/json" -H "Host: validator.${CLIENT}.dappnode" "${WEB3SIGNER_API}/eth/v1/keystores")
-HTTP_CODE=${WEB3SIGNER_RESPONSE: -3}
-CONTENT=$(echo "${WEB3SIGNER_RESPONSE}" | head -c-4)
-
-if [ "${HTTP_CODE}" == "403" ] && [ "${CONTENT}" == "*Host not authorized*" ]; then
-    echo "${CLIENT} is not authorized to access the Web3Signer API. Start without pubkeys"
-elif [ "$HTTP_CODE" != "200" ]; then
-    echo "Failed to get keystores from web3signer, HTTP code: ${HTTP_CODE}, content: ${CONTENT}"
-else
-    PUBLIC_KEYS_WEB3SIGNER=($(echo "${CONTENT}" | jq -r 'try .data[].validating_pubkey'))
-    if [ ${#PUBLIC_KEYS_WEB3SIGNER[@]} -gt 0 ]; then
-        echo "found validators in web3signer, starting vc with pubkeys: ${PUBLIC_KEYS_WEB3SIGNER[*]}"
-        write_validator_definitions
+# MEVBOOST: https://lighthouse-book.sigmaprime.io/builders.html
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" == "true" ]; then
+    echo "MEVBOOST is enabled"
+    MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
+    if curl --retry 5 --retry-delay 5 --retry-all-errors "${MEVBOOST_URL}"; then
+        EXTRA_OPTS="--builder-proposals ${EXTRA_OPTS}"
+    else
+        echo "MEVBOOST is enabled but ${MEVBOOST_URL} is not reachable"
+        curl -X POST -G 'http://my.dappnode/notification-send' --data-urlencode 'type=danger' --data-urlencode title="${MEVBOOST_URL} is not available" --data-urlencode 'body=Make sure the mevboost is available and running'
     fi
 fi
 
@@ -54,7 +23,7 @@ exec -c lighthouse \
     --init-slashing-protection \
     --datadir /root/.lighthouse \
     --beacon-nodes $BEACON_NODE_ADDR \
-    --graffiti="${GRAFFITI:0:32}" \
+    --graffiti="$GRAFFITI" \
     --http \
     --http-address 0.0.0.0 \
     --http-port ${VALIDATOR_PORT} \

5fd12fa7129cd9edb97e7bfac479633cda37d1cc
Voss <voss@visnovalabs.io>
2022-11-11 19:56:27 -0600

    Revert "update graffiti limit"

--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -46,11 +46,6 @@ else
     fi
 fi
 
-oLang=$LANG oLcAll=$LC_ALL
-LANG=C LC_ALL=C 
-graffitiString=${GRAFFITI:0:32}
-LANG=$oLang LC_ALL=$oLcAll
-
 exec -c lighthouse \
     --debug-level=${DEBUG_LEVEL} \
     --network=${NETWORK} \
@@ -59,7 +54,7 @@ exec -c lighthouse \
     --init-slashing-protection \
     --datadir /root/.lighthouse \
     --beacon-nodes $BEACON_NODE_ADDR \
-    --graffiti="${graffitiString}" \
+    --graffiti="${GRAFFITI:0:32}" \
     --http \
     --http-address 0.0.0.0 \
     --http-port ${VALIDATOR_PORT} \
